cf1bf9e36fbcc78e0bae2de8fcc3e938cccc17d4
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-01-25 21:00:45 +0100

    Throw exceptions and be generally unsafe

--- src/Requester.php
+++ src/Requester.php
@@ -7,7 +7,16 @@ class Requester {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_POST, 1);
-        return curl_exec($ch);
+        
+        //Tres unsafe, mon ami!
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        
+        $result = curl_exec($ch);
+        if(!$result) {
+            throw new Exception("Something went wrong while POSTing to $url: ".curl_error($ch));
+        }
+        return $result;
     }
 }
 
