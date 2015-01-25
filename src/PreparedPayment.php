16d1478c70ef2f4a0cea23c9cfe905db241e73ca
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-01-25 21:04:01 +0100

    fix interface

--- src/PreparedPayment.php
+++ src/PreparedPayment.php
@@ -1,7 +1,7 @@
 <?php
 
 abstract class PreparedPayment {
-    abstract protected static function fromSerializedData($serializedData);
+    abstract protected static function fromSerializedData($requester, $serializedData);
     abstract protected function getCheckoutLink();
     abstract protected function getSerializedData();
 }
