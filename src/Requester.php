8f102bef9a4497b44e567209ada4bb34bee58654
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-01-25 20:51:39 +0100

    Naive Requester#post

--- src/Requester.php
+++ src/Requester.php
@@ -1,5 +1,14 @@
 <?php
 
-class Requester {}
+class Requester {
+    public function post($url, $data) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        return curl_exec($ch);
+    }
+}
 
 ?>
