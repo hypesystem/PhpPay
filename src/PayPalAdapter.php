e9da35aa878d2ce8261dc9217b0d57e0f77dc770
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-01-25 14:50:53 +0100

    More paypal adapter stuff

--- src/PayPalAdapter.php
+++ src/PayPalAdapter.php
@@ -1,7 +1,11 @@
 <?php
 
 class PayPalAdapter extends PaymentAdapter {
-    public function __construct($returnUrl, $cancelUrl, $options, $useSandbox = false) {
+    public function __construct($returnUrl, $cancelUrl, $options = array(), $identifyingArgumentName = "id", $useSandbox = false) {
+        $this->requireUrl($returnUrl);
+        $this->requireUrl($cancelUrl);
+        $this->requireOptions($options, array("USER", "PWD", "SIGNATURE"));
+        
         //Handle sandbox/not sandbox
         $this->apiUrl = "https://api-3t.paypal.com/nvp";
         $this->expressCheckoutUrl = "https://www.paypal.com/cgi-bin/webscr";
@@ -10,10 +14,18 @@ class PayPalAdapter extends PaymentAdapter {
             $this->expressCheckoutUrl = "https://www.sandbox.paypal.com/cgi-bin/webscr";
         }
         $this->sandbox = $useSandbox;
+        
+        $this->identifyingArgumentName = $identifyingArgumentName;
+        
+        $this->returnUrl = $returnUrl;
+        $this->cancelUrl = $cancelUrl;
 
         //Get options/reasonable defaults
         $this->options = array(
-            "VERSION" => 109.0
+            "VERSION" => 109.0,
+            "SOLUTIONTYPE" => "Sole",
+            "LANDINGPAGE" => "Billing",
+            "PAYMENTREQUEST_0_PAYMENTACTION" => "Sale"
         );
         foreach($options as $key => $value) {
             $key = strtoupper($key);
@@ -21,9 +33,54 @@ class PayPalAdapter extends PaymentAdapter {
             if($key == "VERSION" && $value != $this->options["VERSION"]) {
                 trigger_error("The PayPalAdapter is configured with a custom version '$value', but the library is only guaranteed to work with '{$this->options["VERSION"]}'.", E_USER_WARNING);
             }
+            if($key == "RETURNURL" || $key == "CANCELURL") {
+                throw new InvalidArgumentException("Cannot set $key in PayPalAdapter. It must be set as an argument in the constructor.");
+            }
 
             $this->options[$key] = $value;
         }
+    }
+    
+    public function preparePayment($identifyingValue, Order $order) {
+        $options = $this->getOptionsWithLineItems($order);
+        $options["RETURNURL"] = $this->composeReturnUrl($identifyingValue);
+        $options["CANCELURL"] = $this->composeCancelUrl($identifyingValue);
+        $options["METHOD"] = "SetExpressCheckout";
+    }
+    
+    private function getOptionsWithLineItems(Order $order) {
+        $options = $this->options;
+        $total = 0;
+        $lineItemCount = $order->getLineCount();
+        for($i = 0; $i < $lineItemCount; $i++) {
+            $item = $order->getLine($i);
+            
+        }
+    }
+    
+    private function composeReturnUrl($id) {
+        return $this->composeUrl($this->returnUrl, array(
+            $this->identifyingArgumentName => $id
+        ));
+    }
+    
+    private function composeUrl($url, $queries) {
+        //Do something clever...
+        return $url.$this->buildQueryString($queries);
+    }
+    
+    private function buildQueryString($queryMap) {
+        $queryStrings = array();
+        foreach($queryMap as $key => $value) {
+            $queryStrings[] = $key."=".$value;
+        }
+        return "?".implode("&",$queryStrings);
+    }
+    
+    private function composeCancelUrl($id) {
+        return $this->composeUrl($this->cancelUrl, array(
+            $this->identifyingArgumentName => $id
+        ));
     }
 }
 
